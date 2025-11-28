49821711eed44648c0aa99b40f7a4d9da14a677a
indrajitkumar23541-a11y <indrajitkumar23541@gmail.com>
2025-11-28 19:10:04 +0530

    My 2nd Program in JAVA

---
+++ Basics/Numbers.java
@@ -0,0 +1,11 @@
+package Basics;
+
+public class Numbers {
+    public static void main(String[] args) {
+        System.out.println("4");
+        System.out.println(4);
+        System.out.println(4+3);
+        System.out.println("4+3");
+    }
+    
+}
