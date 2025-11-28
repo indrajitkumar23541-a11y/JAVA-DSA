50d5e2439b0e446ac2a267df74d3733fcc61797a
indrajitkumar23541-a11y <indrajitkumar23541@gmail.com>
2025-11-28 19:39:29 +0530

    My 3rd Program in JAVA.

---
+++ Basics/Variables.java
@@ -0,0 +1,13 @@
+package Basics;
+
+public class Variables {
+    public static void main(String[] args) {
+        int x = 500;
+        int y = 345;
+        System.out.println(x+y);
+        System.out.println(x-y);
+        System.out.println(x*y);
+        System.out.println(x/y);
+    }
+    
+}
