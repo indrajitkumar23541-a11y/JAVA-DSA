a4fd077512f075dc16339ffb17bd26289b12158d
indrajitkumar23541-a11y <indrajitkumar23541@gmail.com>
2025-11-29 05:23:00 +0530

    Synced with remote

---
+++ Basics/IncrementDecerement.java
@@ -0,0 +1,13 @@
+package Basics;
+
+public class IncrementDecerement {
+    public static void main(String[] args) {
+        int x = 5;
+        x++;
+        int y = 7;
+        y--;
+        System.out.println(x);
+        System.out.println(y);
+    }
+    
+}
