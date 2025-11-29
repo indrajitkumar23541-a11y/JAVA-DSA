b6230167a2437275e3234b66b3477ad92a8dec77
indrajitkumar23541-a11y <indrajitkumar23541@gmail.com>
2025-11-29 10:43:23 +0530

    Calculating Area Of Circle

---
+++ Basics/Calculate_Area.java
@@ -0,0 +1,11 @@
+package Basics;
+
+public class Calculate_Area {
+    public static void main(String[] args) {
+        double r=7;
+        double pi=3.1415;
+        double area = (pi*r*r);
+        System.out.println(area);
+    }
+    
+}
