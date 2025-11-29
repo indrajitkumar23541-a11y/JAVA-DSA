782b780502b543d95f630e658246ee610508e7a5
indrajitkumar23541-a11y <indrajitkumar23541@gmail.com>
2025-11-29 10:25:43 +0530

    Calculating Percentage

---
+++ Basics/Calculating_Percentage.java
@@ -0,0 +1,14 @@
+package Basics;
+
+public class Calculating_Percentage {
+    public static void main(String[] args) {
+        double x1 = 90;
+        double x2 = 91;
+        double x3 = 95;
+        double x4 = 96;
+        double x5 = 97;
+        double y = (x1+x2+x3+x4+x5)/5;
+        System.out.println(y);
+    }
+    
+}
