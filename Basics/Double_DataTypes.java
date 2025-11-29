9260158198a21e561d2e2e82dd6e7ec90487e90a
indrajitkumar23541-a11y <indrajitkumar23541@gmail.com>
2025-11-29 10:07:36 +0530

    Double Data Types Program

---
+++ Basics/Double_DataTypes.java
@@ -0,0 +1,13 @@
+package Basics;
+
+public class Double_DataTypes {
+    public static void main(String[] args) {
+        double x=34;
+        double y=12;
+        System.out.println(x+y);
+        System.out.println(x-y);
+        System.out.println(x*y);
+        System.out.println(x/y);
+    }
+    
+}
