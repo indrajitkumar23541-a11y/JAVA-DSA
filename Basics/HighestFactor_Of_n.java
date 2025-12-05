82d71343cc36a1204b89e364efa87d1e1c9cbb69
indrajitkumar23541-a11y <indrajitkumar23541@gmail.com>
2025-12-05 12:34:05 +0530

    Find the highest factor of number

---
+++ Basics/HighestFactor_Of_n.java
@@ -0,0 +1,21 @@
+package Basics;
+45
+import java.util.Scanner;
+
+public class HighestFactor_Of_n {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter n : ");
+        int n=sc.nextInt();
+        int HF = 1;
+        for(int i=n-1; i>=1; i--){
+            if(n%i==0){
+                HF=i;
+                break;
+            }
+        }
+        System.out.println(HF);
+        sc.close();
+    }
+    
+}
